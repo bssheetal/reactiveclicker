3e01e57e648a453a667bd57fe828e508e65ae89c
SheetalSrikumar <bs.sheetal@gmail.com>
2019-01-23 17:41:28 -0800

    able to retreive images and component baseline structured

--- reactiveclicker/src/App.js
+++ reactiveclicker/src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import images from "./clicky.json"
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    images
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+        {this.state.images.map(element=> (<img src={element.image}></img>))}
+     </div>
     );
   }
 }
